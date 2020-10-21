2b59aa5d3eb93bcf9908008a44d6b12aadf3108d
Nataliya Zhuravleva <natalindria@gmail.com>
2020-10-20 23:35:29 -0700

    Add js dir

---
+++ js/scripts.js
@@ -0,0 +1,79 @@
+$ (document).ready(function(){
+
+  $(".JS").click(function(){ 
+    $(".JS .def1").toggle();
+    $(".JS .term1").toggle();
+  });
+  $(".Operator").click(function(){ 
+    $(".Operator .def1").toggle();
+    $(".Operator .term1").toggle();
+  });
+  $(".Variables").click(function(){ 
+    $(".Variables .def1").toggle();
+    $(".Variables .term1").toggle();
+  });
+  $(".Variable_naming_conventions").click(function(){ 
+    $(".Variable_naming_conventions .def1").toggle();
+    $(".Variable_naming_conventions .term1").toggle();
+  });
+  $(".Functions").click(function(){ 
+    $(".Functions .def1").toggle();
+    $(".Functions .term1").toggle();
+  });
+  $(".Methods").click(function(){ 
+    $(".Methods .def1").toggle();
+    $(".Methods .term1").toggle();
+  });
+  $(".Arguments").click(function(){ 
+    $(".Arguments .def1").toggle();
+    $(".Arguments .term1").toggle();
+  });
+  $(".Parameters").click(function(){ 
+    $(".Parameters .def1").toggle();
+    $(".Parameters .term1").toggle();
+  });
+  $(".Return").click(function(){ 
+    $(".Return .def1").toggle();
+    $(".Return .term1").toggle();
+  });
+  $(".Chaining_methods").click(function(){ 
+    $(".Chaining_methods .def1").toggle();
+    $(".Chaining_methods .term1").toggle();
+  });
+  $(".Strings").click(function(){ 
+    $(".Strings .def1").toggle();
+    $(".Strings .term1").toggle();
+  });
+  $(".Booleans").click(function(){ 
+    $(".Booleans .def1").toggle();
+    $(".Booleans .term1").toggle();
+  });
+  $(".Undefined").click(function(){ 
+    $(".Undefined .def1").toggle();
+    $(".Undefined .term1").toggle();
+  });
+  $(".NaN").click(function(){ 
+    $(".NaN .def1").toggle();
+    $(".NaN .term1").toggle();
+  });
+  $(".Null").click(function(){ 
+    $(".Null .def1").toggle();
+    $(".Null .term1").toggle();
+  });
+  $(".Alert").click(function(){ 
+    $(".Alert .def1").toggle();
+    $(".Alert .term1").toggle();
+  });
+  $(".Comments").click(function(){ 
+    $(".Comments .def1").toggle();
+    $(".Comments .term1").toggle();
+  });
+  $(".jQuery").click(function(){ 
+    $(".jQuery .def1").toggle();
+    $(".jQuery .term1").toggle();
+  });
+  $(".Attributes").click(function(){ 
+    $(".Attributes .def1").toggle();
+    $(".Attributes .term1").toggle();
+  });
+});
